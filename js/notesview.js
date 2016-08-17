ed193ce68804c65d601df98f7ca2da206b0b2a45
Seemant Kulleen <seemantk@gmail.com>
2016-08-16 17:11:15 -0700

    Remove the brush specific actions here, and instead respond to the update() API method instead. Zoomed view needs help.

--- js/notesview.js
+++ js/notesview.js
@@ -3,116 +3,39 @@ function NotesView(){
       , width = 900
       , height = 500
       , margin = { top: 10, bottom: 20, left: 10, right: 10 }
-      , x = d3.scale.linear()
-      , y = d3.scale.linear()
-      , brush = d3.svg.brush()
-          .x(x)
-          .on("brush", brushed)
-      , brushEnabled = false
+      , x = d3.scaleLinear()
+      , y = d3.scaleLinear()
       , tipEnabled = false
-      , onBrush
       , colorScale
       , noteHeight
       , roundedCornerSize
+      , rects
       , dispatch
       , emphasize
-
       // This is a fixed x domain set from outside,
       // it overrides the domain computed from the data.
       // This is set on the focus view based on the brush of the context view.
       , xDomain
+      , x2 // x-axis reference for zoom
       , tip = d3.tip()
           .attr("class", "d3-tip")
-          .html(function(d) { return d.pitchName; });
+          .html(function(d) { return d.pitchName; })
     ;
-
-    function brushed(){
-        if(onBrush){
-            onBrush(brush.empty() ? x.domain() : brush.extent());
-        }
-    }
 
     function my(selection){
         svg = selection
             .attr("height", height)
             .attr("width", width)
         ;
-        svg.selectAll("g")
-            .data(["notes-g", "brush"])
-            .enter()
-              .append("g")
-              .attr("class", function (d){ return d; })
-        ;
-        if(tipEnabled)
-            svg.call(tip)
-        ;
-        var notesG = svg.select(".notes-g")
-          , brushG = svg.select(".brush")
-        ;
-
         svg.each(function(data) {
-            if(xDomain){
-                x.domain(xDomain);
-            } else {
-                x.domain([
-                      d3.min(data, function(d) { return d.time; })
-                    , d3.max(data, function(d) { return d.time + d.duration; })
-                  ])
-                ;
-            }
-            y.domain([
-                  d3.min(data, function(d) { return d.pitch - 1; })
-                , d3.max(data, function(d) { return d.pitch; })
-              ])
+            svg
+              .append("g")
+                .attr("class", "notes-g")
             ;
-            x.range([0, width - 1]);
-            y.range([height, 0]);
-
-            noteHeight = height / (y.domain()[1] - y.domain()[0])
-            roundedCornerSize = noteHeight / 2
-
-            var rects = notesG.selectAll("rect").data(data);
-            rects
-              .enter().append("rect")
-                .attr("class", "note")
-                .classed("subdued", function(d) {
-                    return emphasize && d.voice !== emphasize;
-                  })
-            ;
-            rects.exit().remove();
-            rects
-                .attr("x", function(d) { return x(d.time); })
-                .attr("y", function(d) { return y(d.pitch); })
-                .attr("width", function(d) { return x(d.time + d.duration) - x(d.time); })
-                .attr("height", noteHeight)
-                .attr("fill", function(d) { return colorScale(d.voice); })
-                .attr("stroke", function(d) { return colorScale(d.voice); })
-                .attr("rx", roundedCornerSize)
-                .attr("ry", roundedCornerSize)
-                .classed("subdued", function(d) {
-                    return emphasize && d.voice !== emphasize;
-                  })
-            ;
-
-            if(brushEnabled){
-                rects
-                    .attr("display", function(d) {
-                        return (emphasize && d.voice !== emphasize)
-                          ? "none"
-                          : null
-                        ;
-                      })
-                brushG
-                    .call(brush)
-                  .selectAll("rect")
-                    .attr("y", 0)
-                    .attr("height", height - 1);
-            }
-
+            update(data);
             if(tipEnabled){
-                rects
-                    .on("mouseover", tip.show)
-                    .on("mouseout", tip.hide)
+                if(tipEnabled)
+                    svg.call(tip)
                 ;
             }
         });
@@ -121,6 +44,48 @@ function NotesView(){
     /*
     ** Helper Functions
     */
+    function update(notes) {
+      x.domain([
+            d3.min(notes, function(d) { return d.time; })
+          , d3.max(notes, function(d) { return d.time + d.duration; })
+        ])
+      ;
+      y.domain([
+            d3.min(notes, function(d) { return d.pitch - 1; })
+          , d3.max(notes, function(d) { return d.pitch; })
+        ])
+      ;
+      x.range([0, width - 1]);
+      y.range([height, 0]);
+
+      noteHeight = height / (y.domain()[1] - y.domain()[0])
+      roundedCornerSize = noteHeight / 2
+
+      var rects = svg.select(".notes-g").selectAll("rect")
+          .data(notes)
+      ;
+      rects.exit()
+        .remove()
+      ;
+      rects.enter()
+        .append("rect")
+          .attr("class", "note")
+          .attr("x", function(d) { return x(d.time); })
+          .attr("rx", roundedCornerSize)
+          .attr("y", function(d) { return y(d.pitch); })
+          .attr("ry", roundedCornerSize)
+          .attr("width", function(d) { return x(d.time + d.duration) - x(d.time); })
+          .attr("height", noteHeight)
+          .attr("fill", function(d) { return colorScale(d.voice); })
+          .attr("stroke", function(d) { return colorScale(d.voice); })
+          .on("mouseover", tip.show)
+          .on("mouseout", tip.hide)
+          .classed("subdued", function(d) {
+              return emphasize && d.voice !== emphasize;
+            })
+      ;
+    } // update()
+
     function hilite(arg) {
         emphasize = arg.emphasize;
 
@@ -131,6 +96,17 @@ function NotesView(){
         ;
     } // hilite()
 
+    /*
+    ** API (Getter) Functions
+    */
+    my.x = function() {
+        return x;
+      } // my.x()
+    ;
+    my.y = function() {
+        return y;
+      } // my.y()
+    ;
     /*
     ** API (Getter/Setter) Functions
     */
@@ -161,14 +137,6 @@ function NotesView(){
         return my;
     };
 
-    my.onBrush = function (callback){
-        onBrush = callback;
-    };
-
-    my.xDomain = function (value){
-        xDomain = value;
-    };
-
     my.tipEnabled = function (value){
         if(arguments.length === 0) return tipEnabled;
         tipEnabled = value;
@@ -177,14 +145,19 @@ function NotesView(){
     ;
     my.connect = function(value){
         if(!arguments.length) return dispatch;
-        var name = brushEnabled ? "focus" : "context";
 
+        var name = svg.attr("id");
         dispatch = value
             .on("hilite.notesview" + name, hilite) // listen for appropriate events
         ;
 
         return my;
       } // my.connect()
+    ;
+    my.update = function(dataset) {
+        if(!arguments.length) return my;
+        update(dataset);
+      } // my.update()
     ;
     return my;
 } // NotesView()
