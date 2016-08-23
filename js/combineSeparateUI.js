c3f6277eb4e79f4a878b62a361bb8cc1d3734b6d
Seemant Kulleen <seemantk@gmail.com>
2016-08-22 20:13:56 -0700

    Convert this to reusable chart pattern, even though it isn't a chart, and renders regular HTML objects.

--- js/combineSeparateUI.js
+++ js/combineSeparateUI.js
@@ -1,34 +1,45 @@
 function CombineSeparateUI(){
-
-    var container = d3.select("#combine-separate-ui")
-      , form = container.append("form").attr("class", "form")
+    /*
+    ** Private Variables
+    */
+    var form
       , data = ["Combine", "Separate"]
       , dispatch
     ;
-
-    var labels = form.selectAll("label").data(data)
-          .enter().append("label")
-            .attr("class", "radio-inline")
-      , inputs = labels.append("input")
-            .attr("type", "radio")
-            .attr("name", "combine-separate-group")
-      , spans = labels.append("span")
-            .text(function (d){ return " " + d + " "; })
-    ;
-
-    inputs.on("click", function (e){
-        dispatch.separate(e == "Separate");
-    });
-
-    function my() {
-    } // my()
-
+    /*
+    ** Main Function Object
+    */
+    function my(sel) {
+        form = sel
+          .append("form")
+            .attr("class", "form")
+        ;
+        var labels = form.selectAll("label")
+                .data(data)
+              .enter().append("label")
+                .attr("class", "radio-inline")
+          , inputs = labels
+              .append("input")
+                .attr("type", "radio")
+                .attr("name", "combine-separate-group")
+                .property("checked", function(d, i) { return !i; })
+          , spans = labels
+              .append("span")
+                .text(function (d){ return " " + d + " "; })
+        ;
+        inputs.on("click", function (e){
+            dispatch.separate(e == "Separate");
+        });
+    } // my() - Main Function Object
+    /*
+    ** API (Getters/Setters)
+    */
     my.connect = function(value){
         if(!arguments.length) return dispatch;
         dispatch = value;
         return my;
       } // my.connect()
     ;
-
+    // ALWAYS return this last
     return my;
-}
+} // CombineSeparateUI()
