7ac7b1430d23ed336086b6b3dc94f08a54a26e7d
Seemant Kulleen <seemantk@gmail.com>
2016-08-24 09:55:47 -0700

    Respace long line

--- js/notescanvas.js
+++ js/notescanvas.js
@@ -101,7 +101,9 @@ function NotesCanvas(){
         selection.selectAll("rect.note")
             .attr("x", function(d) { return scale.zoom.x(d.time); })
             .attr("y", function(d) { return scale.zoom.y(d.pitch); })
-            .attr("width", function(d) { return scale.zoom.x(d.time + d.duration) - scale.zoom.x(d.time); })
+            .attr("width", function(d) {
+                return scale.zoom.x(d.time + d.duration) - scale.zoom.x(d.time);
+              })
             .attr("height", noteHeight)
             .attr("fill", function(d) { return colorScale(d.voice); })
             .attr("stroke", function(d) { return colorScale(d.voice); })
