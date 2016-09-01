fc9fe6fdb8e004b3e191f6c7a10d97258d041a80
curran <curran.kelleher@gmail.com>
2016-09-01 12:05:54 +0530

    Add extremeNotesUI. Starting towards #45

---
+++ js/extremeNotesUI.js
@@ -0,0 +1,43 @@
+function ExtremeNotesUI(){
+    /*
+    ** Private Variables
+    */
+    var form
+      , data = ["Show Extreme Notes"]
+      , dispatch
+    ;
+    /*
+    ** Main Function Object
+    */
+    function my(sel) {
+        form = sel
+          .append("form")
+            .attr("class", "form")
+        ;
+        var labels = form.selectAll("label")
+                .data(data)
+              .enter().append("label")
+          , inputs = labels
+              .append("input")
+                .attr("type", "checkbox")
+                .property("checked", true)
+          , spans = labels
+                .text(function (d){ return " " + d + " "; })
+        ;
+        inputs.on("click", function (e){
+            dispatch.extremes(this.checked);
+          })
+        ;
+    } // my() - Main Function Object
+    /*
+    ** API (Getters/Setters)
+    */
+    my.connect = function(value){
+        if(!arguments.length) return dispatch;
+        dispatch = value;
+        return my;
+      } // my.connect()
+    ;
+    // ALWAYS return this last
+    return my;
+} // ExtremeNotesUI()
