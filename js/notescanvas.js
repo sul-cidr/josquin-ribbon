7083a4a7c529a64065b0b745a795d6d30ed3bae3
curran <curran.kelleher@gmail.com>
2016-09-04 21:54:50 +0530

    Render note reference lines

--- js/notescanvas.js
+++ js/notescanvas.js
@@ -13,6 +13,8 @@ function NotesCanvas() {
       , dispatch
       , state = true // on; false = off
       , extremes = false
+      , reflinesAxis = d3.axisLeft()
+      , reflines
     ;
     /*
     ** Main Function Object
@@ -35,6 +37,14 @@ function NotesCanvas() {
 
         svg = selection
               .attr("class", "notes-g " + data.key)
+        ;
+        reflinesAxis
+            .scale(scale.y)
+            .tickSize(-width)
+        ;
+        reflines = svg
+          .append("g")
+            .attr("class", "reflines")
         ;
         var rects = svg.selectAll("rect").data(data.value);
         rects
@@ -79,6 +89,7 @@ function NotesCanvas() {
               })
         ;
         hilite();
+        reflines.call(reflinesAxis);
     } // update()
 
     function computeExtremeNotes() {
