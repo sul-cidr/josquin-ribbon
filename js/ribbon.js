9877971c42705fbe9a4ce8eb6aa77fb209e1eae8
curran <curran.kelleher@gmail.com>
2016-09-27 19:43:08 +0530

    Add stub for Ribbon implementation

---
+++ js/ribbon.js
@@ -0,0 +1,6 @@
+function Ribbon() {
+    var interval = 6 // The interval size for the sliding window, in units of beats.
+      , step = 1 // How much to slide the window for each iteration.
+      , bandwidth = 1 // The scaling factor multiplied by the standard deviation before adding/subtracting from the mean.
+    ;
+}
