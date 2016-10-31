f1dd5f9f1f053ca65b61504db1ab4a9f0adb32fd
Seemant Kulleen <seemantk@gmail.com>
2016-10-30 22:25:58 -0700

    Simplify: define a structure for the buttons, then create DOM elements. The toggle button is the always visible button.  The buttons in the flyout element are meant to appear/disappear depending on the toggle button's state.

--- js/ribbonsUI.js
+++ js/ribbonsUI.js
@@ -3,99 +3,63 @@ function RibbonsUI(){
     ** Private Variables
     */
     var div
-      , labelText = "Show Ribbons"
-      , showRibbons = true
-      , ribbonMode = Ribbon.STANDARD_DEVIATION
-      , modes = [
-          {
-            label: "Standard Deviation"
-            , mode: Ribbon.STANDARD_DEVIATION
-          },
-          {
-            label: "Attack Density"
-            , mode: Ribbon.ATTACK_DENSITY
+      , toggle = {
+              label: "Toggle Ribbons"
+            , icon: "bookmark"
+            , callback: "toggleRibbons"
           }
-        ]
+      , flyout = [
+              {
+                  label: "Standard Deviation"
+                , icon: "tune"
+                , callback: "ribbonMode"
+              }
+            , {
+                  label: "Attack Density"
+                , icon: "multiline_chart"
+                , callback: "ribbonMode"
+              }
+          ]
       , dispatch
     ;
     /*
     ** Main Function Object
     */
     function my() {
-        var cbform = div.select(".panel-body").selectAll("div").data([1])
-          , cbformEnter = cbform.enter().append("div")
-                .attr("class", "ribbon-checkbox checkbox")
+        var toolbar = div.selectAll(".fixed-action-button")
+            .data([toggle], function(d) { return d.label; })
         ;
-        cbform = cbformEnter.merge(cbform);
-
-        /**
-         * Checkbox
-         */
-        var label = cbform.selectAll("label").data([1])
-              .enter().append("label")
+        toolbar = toolbar.enter()
+          .append("div")
+            .attr("class", "fixed-action-button")
         ;
-        label
-            .attr("class", "btn btn-default btn-sm")
-          .append("input")
-            .attr("type", "checkbox")
-            .property("checked", showRibbons)
-            .on("change", function (e){
-                showRibbons = this.checked;
-                dispatch.call("showRibbons", this, showRibbons);
-                label.classed("active", showRibbons);
-                my();
+        toolbar
+          .append("button")
+            .attr("class", "mdl-button mdl-button--fab mdl-button--primary")
+            .on("click", function(d) {
+                var self = this;
+                dispatch.call(d.callback, self, d.label);
               })
+          .append("i")
+            .attr("class", "material-icons")
+            .text(function(d) { return d.icon; })
         ;
-        label
-          .append("span")
-            .text(" " + labelText)
-        ;
-
-        /**
-         * Radio Buttons
-        **/
-        var rdform = div.select(".panel-footer").selectAll("div").data([1])
-          , rdformEnter = rdform.enter().append("div")
-                .attr("class", "ribbon-radio-buttons")
-                .classed("btn-group", true)
-                .attr("data-toggle", "buttons")
-        ;
-        rdform = rdformEnter.merge(rdform);
-        // Hide the radio buttons if the "show ribbons" box is not checked.
-        rdform.style("display", showRibbons ? null : "none");
-
-        var radioLabel = rdform.selectAll("label")
-              .data(modes);
-
-        var radioLabelEnter = radioLabel
-            .enter().append("label")
-              .attr("class", "btn btn-sm btn-default")
-              .classed("active", function (d, i){
-                  return d.mode === ribbonMode;
-                })
-          , inputs = radioLabelEnter
-              .append("input")
-                .attr("type", "radio")
-                .attr("name", "ribbon-mode-group")
-                .property("checked", function(d, i) {
-                    return d.mode === ribbonMode;
-                  })
-          , spans = radioLabelEnter
-              .append("span")
-                .html(function (d){ return d.label; })
-        ;
-        radioLabel = radioLabelEnter.merge(radioLabel);
-        
-        inputs.on("click", function (d){
-            var self = this;
-            ribbonMode = d.mode;
-            radioLabel
-                .classed("active", function(e) {
-                    return this === self.parentNode;
-                  })
-            ;
-            dispatch.call("ribbonMode", self, ribbonMode);
-          })
+        toolbar
+          .append("ul")
+            .attr("class", "list-inline")
+          .selectAll("li")
+            .data(flyout, function(d) { return d.label; })
+          .enter()
+          .append("li")
+          .append("button")
+            .attr("class", "mdl-button mdl-button--fab mdl-button--mini-fab mdl-button--colored")
+            .on("click", function(d) {
+                var self = this;
+                dispatch.call(d.callback, self, d.label);
+              })
+          .append("i")
+            .attr("class", "material-icons")
+            .text(function(d) { return d.icon; })
         ;
     } // my() - Main Function Object
     /*
