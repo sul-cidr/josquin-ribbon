ee2198de37fecccad6f4c074e2ac4b4e0e013155
Seemant Kulleen <seemantk@gmail.com>
2016-11-09 02:36:05 -0800

    Remove scale and domain, in favour of x and y (to receive the full scales there).  Also, move the two compute functions to be values in an object (addressabble by the name of the mode).  Also, both modes will be rendered.  The caller/user of the `<symbol>` will need to figure out visibilty and colors, etc.

--- js/ribbon.js
+++ js/ribbon.js
@@ -1,21 +1,20 @@
 function Ribbon() {
     /* Private Variables */
-    var g
+    var svg
       , data // The original notes data
       , ribbonData // The computed data for the ribbon
       , interval = 12 // Interval size for the sliding window, in units of beats
       , step = 1 // How much to slide the window for each iteration.
       , bandwidth = 1 // Scaling factor * std.dev. before +ing/-ing from mean.
-      , scale // Scale data passed down from notescanvas
-      , domain // Domain data passed down from notescanvas
+      , x, y
       , area = d3.area()
-          .x(function (d){ return scale.x(d.x); })
-          .y0(function (d){ return scale.yLinear(d.y0); })
-          .y1(function (d){ return scale.yLinear(d.y1); })
+          .x(function (d){ return x(d.x); })
+          .y0(function (d){ return y(d.y0); })
+          .y1(function (d){ return y(d.y1); })
           .curve(d3.curveBasis)
       , ribbonData
       , ribbonDataStale = true
-      , mode = Ribbon.STANDARD_DEVIATION
+      , modes = {}
     ;
 
 
@@ -23,102 +22,99 @@ function Ribbon() {
     ** Main Function Object
     */
     function my(){
-        if!(data && domain && scale) return;
-        if(ribbonDataStale){
-            ribbonData = computeRibbon();
-            ribbonDataStale = false;
-        }
-        var path = g.selectAll("path").data([ribbonData])
-        path.exit().remove()
-        path.enter().append("path").merge(path)
-            .attr("class", "ribbon")
-            .style("color", function(d) {
-                return scale.color(data.key);
-              })
-            .attr("d", area)
+        var ribbon = svg.selectAll("g")
+                .data(d3.entries(modes), function(d) { return d.key; })
+        ;
+        ribbon.exit().remove();
+        ribbon
+          .enter().append("g")
+            .attr("class", function(d) { return d.key.toLowerCase(); })
+          .each(function(d) {
+              var path = d3.select(this).selectAll("path")
+                  .data([d])
+              ;
+              return;
+              path
+                .enter().append("path")
+                .merge(path)
+                    .attr("d", function(e) { return area(e.value(d.value)); })
+              ;
+            })
         ;
     } // Main Function Object
 
-    function computeRibbon(){
-      if(mode === Ribbon.STANDARD_DEVIATION){
-        return computeWindowedStandardDeviation();
-      } else if(mode === Ribbon.ATTACK_DENSITY){
-        return computeAttackDensity();
-      }
-    }
+    modes.STANDARD_DEVIATION = function() {
+        console.log(data);
+        // For steps in which there are no notes in the interval,
+        // An empty interval at the previous average is used.
+        var previousMean = data[0].pitch;
 
-    function computeWindowedStandardDeviation(){
+        return d3.range(x.domain()[0], x.domain()[1], step)
+          .map(function (x){
 
-      // For steps in which there are no notes in the interval,
-      // An empty interval at the previous average is used.
-      var previousMean = data.value[0].pitch;
+            // This is a "windowed" computation, so we need to look
+            // at the notes in the window of width `interval`, centered on `x`.
+            var notesInWindow = data
+              .filter(function(d){
 
-      return d3.range(domain.x[0], domain.x[1], step)
-        .map(function (x){
+                // Consider the interval to be centered on the x value.
+                var windowStart = x - interval / 2
+                  , windowEnd   = x + interval / 2
+                  , noteStart = d.time
+                  , noteEnd   = d.time + d.duration
+                ;
 
-          // This is a "windowed" computation, so we need to look
-          // at the notes in the window of width `interval`, centered on `x`.
-          var notesInWindow = data.value
-            .filter(function(d){
+                // Consider a note to be "inside the window"
+                // if any part of it falls inside the window.
+                return (noteStart < windowEnd) && (noteEnd > windowStart);
+              })
+              .map(function (d){ return d.pitch; })
+            ;
 
-              // Consider the interval to be centered on the x value.
-              var windowStart = x - interval / 2
-                , windowEnd   = x + interval / 2
-                , noteStart = d.time
-                , noteEnd   = d.time + d.duration
-              ;
+            // At each iteration of this function,
+            // we'll compute the mean and standard deviation.
+            var mean, deviation;
 
-              // Consider a note to be "inside the window"
-              // if any part of it falls inside the window.
-              return (noteStart < windowEnd) && (noteEnd > windowStart);
-            })
-            .map(function (d){ return d.pitch; })
-          ;
+            // The mean and standard deviation values will be computed
+            // differently depending on how many notes are in our window.
+            switch(notesInWindow.length){
 
-          // At each iteration of this function,
-          // we'll compute the mean and standard deviation.
-          var mean, deviation;
+              // If the set of notes in our current window is empty,
+              // then use the previous mean and a deviation of 0.
+              case 0:
+                mean = previousMean;
+                deviation = 0;
+                break;
 
-          // The mean and standard deviation values will be computed
-          // differently depending on how many notes are in our window.
-          switch(notesInWindow.length){
+              // If there's only a single note in our window,
+              // then use its pitch as the mean, and a deviation of 0.
+              case 1:
+                mean = notesInWindow[0];
+                deviation = 0;
+                break;
 
-            // If the set of notes in our current window is empty,
-            // then use the previous mean and a deviation of 0.
-            case 0:
-              mean = previousMean;
-              deviation = 0;
-              break;
+              // If there's more than 1 note in our window,
+              // then compute the mean and standard deviation in earnest.
+              default:
+                deviation = d3.deviation(notesInWindow);
+                mean = d3.mean(notesInWindow);
+            }
 
-            // If there's only a single note in our window,
-            // then use its pitch as the mean, and a deviation of 0.
-            case 1:
-              mean = notesInWindow[0];
-              deviation = 0;
-              break;
+            // Stash the previous average for next time around,
+            // whatever it may be, for use in the case that
+            // the set of notes in the window is empty.
+            previousMean = mean;
 
-            // If there's more than 1 note in our window,
-            // then compute the mean and standard deviation in earnest.
-            default:
-              deviation = d3.deviation(notesInWindow);
-              mean = d3.mean(notesInWindow);
-          }
+            // Return an objects that represents this slice of the ribbon.
+            return {
+                x: x
+              , y1: mean + deviation
+              , y0: mean - deviation
+            };
+          });
+    } // modes.STANDARD_DEVIATION()
 
-          // Stash the previous average for next time around,
-          // whatever it may be, for use in the case that
-          // the set of notes in the window is empty.
-          previousMean = mean;
-
-          // Return an objects that represents this slice of the ribbon.
-          return {
-              x: x
-            , y1: mean + deviation
-            , y0: mean - deviation
-          };
-        });
-    }
-
-    function computeAttackDensity(){
+    modes.ATTACK_DENSITY = function() {
 
       // Use the following fixed values for the attack density computation,
       // as these specific values were prescribed by Josquin project leads.
@@ -197,14 +193,14 @@ function Ribbon() {
             , y0: mean - density
           };
         });
-    }
+    } // modes.ATTACK_DENSITY()
 
-    my.g = function(value) {
+    my.svg = function(value) {
         if(!arguments.length)
-            return g;
-        g = value;
+            return svg;
+        svg = value;
         return my;
-      } // my.g()
+      } // my.svg()
     ;
     my.data = function(value) {
         if(!arguments.length)
@@ -237,22 +233,6 @@ function Ribbon() {
         ribbonDataStale = true;
         return my;
       } // my.bandwidth()
-    ;
-    my.scale = function(value) {
-        if(!arguments.length)
-            return scale;
-        scale = value;
-        ribbonDataStale = true;
-        return my;
-      } // my.scale()
-    ;
-    my.domain = function(value) {
-        if(!arguments.length)
-            return domain;
-        domain = value;
-        ribbonDataStale = true;
-        return my;
-      } // my.domain()
     ;
     my.mode = function(value) {
         if(!arguments.length)
