5405a39b893bde61a03b051426e0dfb71d1f779c
Seemant Kulleen <seemantk@gmail.com>
2016-11-24 01:17:54 -0800

    Clean up.

--- js/toggleUI.js
+++ js/toggleUI.js
@@ -16,30 +16,29 @@ function ToggleUI() {
             .attr("class", "mdl-switch mdl-js-switch")
             .attr("for", function(d, i) { return "switch-" + d.callback; })
             .each(function(d, i) {
-                d3.select(this)
-                  .append("input")
-                    .attr("type", "checkbox")
-                    .attr("id", "switch-" + d.callback)
-                    .attr("class", "mdl-switch__input")
-                    .on("change", function() {
-                        var ret = d.values
-                              ? (this.checked ? d.values[0] : d.values[1])
-                              : this.checked
-                        ;
-                        dispatch.call(d.callback, this, ret);
-                      })
+                var self = d3.select(this)
+                  , check = self.append("input")
+                        .attr("type", "checkbox")
+                        .attr("id", "switch-" + d.callback)
+                        .attr("class", "mdl-switch__input")
+                  , label = self.append("span")
+                        .attr("class", "mdl-switch__label")
                 ;
-                d3.select(this)
-                  .append("span")
-                    .attr("class", "mdl-switch__label")
-                  .append("i")
-                    .attr("class", "material-icons")
-                    .text(d.icon)
+                if(d.icon) {
+                    label.append("i")
+                        .attr("class", "material-icons")
+                        .text(d.icon)
+                    ;
+                }
+                componentHandler.upgradeElement(sel.select(".mdl-switch").node());
+                check
+                    .on("change", function() {
+                        dispatch.call(d.callback, this, this.checked);
+                      })
                 ;
               })
             .merge(toggle)
         ;
-        componentHandler.upgradeElement(sel.select(".mdl-switch").node());
     } // Main Function Object
 
     /*
