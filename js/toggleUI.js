2fc5cd26c5caa5745b9bf400859e3f5e40b7609c
Seemant Kulleen <seemantk@gmail.com>
2016-11-24 01:35:37 -0800

    Convert the options into an array of key/value pairs.  Build the select box, and choose the first option as the default. The toggle switch enables/disables the dropdown box before calling the signal dispatch. When the toggle is on, the dropdown is enabled.

--- js/toggleUI.js
+++ js/toggleUI.js
@@ -23,17 +23,41 @@ function ToggleUI() {
                         .attr("class", "mdl-switch__input")
                   , label = self.append("span")
                         .attr("class", "mdl-switch__label")
+                  , options = d3.entries(d.options)
                 ;
                 if(d.icon) {
                     label.append("i")
                         .attr("class", "material-icons")
                         .text(d.icon)
                     ;
+                } else if(options.length) {
+                    label.append("select")
+                        .on("change", function() {
+                            dispatch.call(d.callback, this, this.value);
+                          })
+                      .selectAll("option")
+                        .data(options, function(o) { return o.key; })
+                      .enter().append("option")
+                        .attr("default", function(d, i) { return !i; }) // 1st
+                        .attr("value", function(o) { return o.key; })
+                        .text(function(o) { return o.value; })
+                    ;
                 }
                 componentHandler.upgradeElement(sel.select(".mdl-switch").node());
                 check
                     .on("change", function() {
-                        dispatch.call(d.callback, this, this.checked);
+                        var checked = this.checked
+                          , node = label.select("select").node()
+                          , value = checked && node
+                                  ? node.value
+                                  : checked
+                        ;
+                        if(options.length) {
+                            label.select("select")
+                                .attr("disabled", checked ? null : "disabled")
+                            ;
+                        }
+                        dispatch.call(d.callback, this, value);
                       })
                 ;
               })
