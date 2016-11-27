180aeb8f12ee6125b169a23312f04fad1253dc31
Seemant Kulleen <seemantk@gmail.com>
2016-11-27 15:38:01 -0800

    simplify from d3.map to a plain object.  Add each voice's pitch extent into the notes metadata.

--- js/baton.js
+++ js/baton.js
@@ -39,7 +39,7 @@ d3.queue()
 ;
 
 function parseJSON(proll) {
-    var remix = d3.map(); // new container for notes data
+    var remix = {}; // new container for notes data
 
     proll.partdata.forEach(function(part) {
         var voice = proll.partnames[part.partindex]
@@ -56,9 +56,14 @@ function parseJSON(proll) {
                 , extreme: pitch === extent[0] || pitch === extent[1]
             });
         });
-        remix.set(voice, { index: part.partindex, notes: notes });
+        remix[voice] = {
+              index: part.partindex
+            , notes: notes
+            , range: d3.extent(notes, function(d) { return d.pitch; })
+          }
+        ;
     });
-    proll.notes = remix.entries()
+    proll.notes = d3.entries(remix)
         .sort(function(a, b) {
             return d3.ascending(a.value.index, b.value.index);
           })
