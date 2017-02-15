9b8095cfc72db844aa7439a19912d6027c9faeb4
Seemant Kulleen <seemantk@gmail.com>
2017-02-14 23:46:03 -0800

    Move all the dev files into a subdirectory, and concatenate them into josquin.js using jekyll.  Thus, the index.html file only needs to include the one josquin.js file -- upcoming commit.

---
+++ js/josquin.js
@@ -0,0 +1,12 @@
+---
+---
+{% include_relative dev/legend.js %}
+{% include_relative dev/reflines.js %}
+{% include_relative dev/ribbon.js %}
+{% include_relative dev/score.js %}
+{% include_relative dev/notescanvas.js %}
+{% include_relative dev/notesbook.js %}
+{% include_relative dev/notesnav.js %}
+{% include_relative dev/toggleUI.js %}
+{% include_relative dev/notesUI.js %}
+{% include_relative dev/baton.js %}
