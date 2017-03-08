4fc3c959dbfe40e29663bb3bb07023a7c5155fae
curran <curran.kelleher@gmail.com>
2017-03-08 19:36:05 +0530

    Add svgsaver lib

--- js/vendors.js
+++ js/vendors.js
@@ -5,3 +5,4 @@
 {% include_relative vendor/jquery.min.js %}
 {% include_relative vendor/bootstrap.min.js %}
 {% include_relative vendor/material.min.js %}
+{% include_relative vendor/svgsaver.min.js %}
