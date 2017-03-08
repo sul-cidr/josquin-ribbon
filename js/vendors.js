cbdf5105a6a1f08c0b83166c297e5835c1b35928
curran <curran.kelleher@gmail.com>
2017-03-08 20:40:30 +0530

    Fix Jekyl error for material js file

--- js/vendors.js
+++ js/vendors.js
@@ -4,5 +4,4 @@
 {% include_relative vendor/d3-tip.min.js %}
 {% include_relative vendor/jquery.min.js %}
 {% include_relative vendor/bootstrap.min.js %}
-{% include_relative vendor/material.min.js %}
 {% include_relative vendor/svgsaver.min.js %}
