13119f29174cf3587deb1fe00b4c710886515fe8
curran <curran.kelleher@gmail.com>
2017-06-06 21:11:09 +0530

    Refactor, rename, add comments

--- js/dev/measureScaling.js
+++ js/dev/measureScaling.js
@@ -10,48 +10,35 @@ var measureScaling = (function (){
 
     // Each entry in the returned array corresponts to a single measure,
     // and the value is the number of beats in that measure.
-    function computeMeasuresToBeats(proll, mensurationsLUT){
+    function annotateMeasures(proll, mensurationsLUT){
 
         // Keeps track of the last seen mensuration value.
         var mensuration;
 
-        var beatsPerMeasure = generateBeatMap(mensurationsLUT);
+        // Create an index from the lookup table.
+        var mensurationsToBeats = {};
+        mensurationsLUT.forEach(function (d){
+            mensurationsToBeats[d.sign] = d.num_quarter_notes;
+        });
 
         return proll.barlines.map(function (d, i){
 
             // Fill in "undefined" mensuration values with last seen value.
             mensuration = (d.mensuration || mensuration);
 
+            // Return an "annotated measure" object, that contains
+            // useful information about each measure based on information
+            // from the mensurationsLUT.
             return {
                 mensuration: mensuration,
-                numBeats: beatsPerMeasure(mensuration)
+                numBeats: mensurationsToBeats[mensuration]
             };
         });
     }
 
-    // Converts from mensuration symbols to their numeric time signature equivalents.
-    function generateBeatMap(mensurationsLUT){
-        var mensurationsToBeats = {};
-        mensurationsLUT.forEach(function (d){
-            mensurationsToBeats[d.sign] = d.num_quarter_notes;
-        });
-        return function beatsPerMeasure(mensuration){
-            var beats = mensurationsToBeats[mensuration];
-            if(beats){
-                return beats;
-            } else {
-
-                // Flag unknown mensuration values, for developers to
-                // cover all cases over time.
-                console.warn("Unknown mensuration value: " + mensuration);
-
-                return 8;
-            }
-        }
-    }
 
     return function (proll, mensurationsLUT){
-        var measuresToBeats = computeMeasuresToBeats(proll, mensurationsLUT),
+        var measuresToBeats = annotateMeasures(proll, mensurationsLUT),
 
             // Maps the beat (quarter note) to absolute time.
             beatsToTime = [],
@@ -65,6 +52,7 @@ var measureScaling = (function (){
 
         measuresToBeats.forEach(function (d){
             var numBeats = d.numBeats;
+            
             //console.log(i + " (measure)");
 
             // Old school for loop to save on Array and closure allocations.
