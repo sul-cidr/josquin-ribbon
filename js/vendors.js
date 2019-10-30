17e846c9753c18a67cbe13c012981cdec033fead
Simon Wiles <simonjwiles@gmail.com>
2019-10-30 14:49:05 -0700

    Drop Bootstrap Javascript
    This seems to affect precisely nothing?

--- js/vendors.js
+++ js/vendors.js
@@ -3,5 +3,4 @@
 {% include_relative vendor/d3.min.js %}
 {% include_relative vendor/d3-tip.min.js %}
 {% include_relative vendor/jquery.min.js %}
-{% include_relative vendor/bootstrap.min.js %}
 {% include_relative vendor/svgsaver.min.js %}
