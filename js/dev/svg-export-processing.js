415f157c89058bdbecf37266e17d0021114499e9
Simon Wiles <simonjwiles@gmail.com>
2019-12-17 12:52:56 -0800

    Better support for SVG prettifying on export

--- js/dev/svg-export-processing.js
+++ js/dev/svg-export-processing.js
@@ -1,25 +1,30 @@
 /* exported prettifyXml, cleanSvg, addSvgPadding, processSymbols, safariNamespaceFix */
 var prettifyXml = function(sourceXml) {
-  if (!XSLTProcessor || !XSLTProcessor.importStylesheet) return sourceXml;
-  var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
-  var xsltDoc = new DOMParser().parseFromString([
-    '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
-    '  <xsl:strip-space elements="*"/>',
-    '  <xsl:template match="para[content-style][text()]">',
-    '    <xsl:value-of select="normalize-space(.)"/>',
-    '  </xsl:template>',
-    '  <xsl:template match="node()|@*">',
-    '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
-    '  </xsl:template>',
-    '  <xsl:output indent="yes"/>',
-    '</xsl:stylesheet>',
-  ].join('\n'), 'application/xml');
 
-  var xsltProcessor = new XSLTProcessor();    
-  xsltProcessor.importStylesheet(xsltDoc);
-  var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
-  var resultXml = new XMLSerializer().serializeToString(resultDoc);
-  return resultXml;
+  try {  
+    var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
+    var xsltDoc = new DOMParser().parseFromString([
+      '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+      '  <xsl:strip-space elements="*"/>',
+      '  <xsl:template match="para[content-style][text()]">',
+      '    <xsl:value-of select="normalize-space(.)"/>',
+      '  </xsl:template>',
+      '  <xsl:template match="node()|@*">',
+      '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+      '  </xsl:template>',
+      '  <xsl:output indent="yes"/>',
+      '</xsl:stylesheet>',
+    ].join('\n'), 'application/xml');
+    
+    var xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsltDoc);
+    var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+    var resultXml = new XMLSerializer().serializeToString(resultDoc);
+    return resultXml;
+  } 
+  catch(error) {
+    return sourceXml;
+  }
 };
 
 var _roundValue = function(valueAsString) {
